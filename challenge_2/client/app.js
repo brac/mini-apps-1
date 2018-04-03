23e257ae10e7f45a3fa244b7bd037cf4cc4ddbdf
Moyeong Lee <2000prcs@gmail.com>
2018-04-03 14:27:01 -0700

    GET / POST between client and server + Render data to index.html
    - GET / POST requests work between client and server
    - Client is able to render data to index.html

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,78 @@
 
+$(document).ready(()=>{
+    
+    // Set variable for original user input and converted csv data
+    var input;
+    var csvData;
+
+    // Get user's input
+    $('#submit').on('click', (event)=>{
+        event.preventDefault();
+        input = $('textarea').val();
+        console.log('input: ', input);
+        //console.log(typeof input);
+        app.send(input);
+    });
+
+    var app = {
+        
+        server: 'http://127.0.0.1:3000/mo__csv_generator',
+        headers: '',
+
+        // POST the input to the server (JS format)
+        send: (input) => {
+            $.ajax({
+                type: 'POST',
+                url: app.server,
+                data: input, 
+                contentType: 'application/json',
+                success: (data) => {
+                    console.log('POST: data from server', data);
+                    console.log('Data sent');
+                    app.fetch(data);
+                },
+                error: (error) => {
+                    console.log('POST failed');
+                }
+            });
+        }, 
+
+        // GET CSV data from the server
+        fetch: (data) => {
+            $.ajax({
+                type: 'GET',
+                url: app.server,
+                data: data,
+                success: (data) => {
+                    console.log('GET: data from server:', data);
+                    console.log('Data received');
+                    csvData = data;
+                    app.render(data);
+
+                }, 
+                error: (error) => {
+                    console.log('GET Failed');
+                }
+            });
+        },
+
+        render: (data) => {
+
+            var dataArray = data.split(',');
+
+            var ths = $('th');
+            
+            for(var i = 0; i < ths.length; i++){
+                $(ths[i]).text(dataArray[i]); 
+            }
+
+        }
+    }
+
+
+});
+
+
+
+
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,58 @@
+const express = require('express');
+const app = express();
 
 
+//To parse body
+var parser = require('body-parser');
+app.use(parser.json());
+
+var csv = '';
+
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", 
+    "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+  // Serve the client files
+app.use(express.static(__dirname + '../client'));
+
+// POST request
+app.post('/mo__csv_generator', (req, res) => {
+    console.log('req.body:', req.body);
+    
+    // Covert JSON object to CSV format
+
+    // Get first row for the table (ex. firstname, lastname, country...)
+    var firstRow = Object.keys(req.body);
+    getTitles(firstRow);
+
+    // Get actual data for the table 
+    var restOfData = Object.keys(req.body).map((key)=> {return req.body[key];});
+    getData(restOfData);
+
+    // need to send as JS format
+    res.send(csv);
+});
+
+var getTitles = (array) => {
+    console.log('header data:', array);
+    var arrayToString = array.join(',');
+    csv += arrayToString;
+    console.log('csv:', csv);
+}
+
+var getData = (array) => {
+    console.log('the rest of data:', array);
+}
+
+// GET request
+app.get('/mo__csv_generator', (req, res) => {
+     res.send(csv);
+});
+
+
+
+
+app.listen(3000, ()=> console.log('Is it listening?'));
